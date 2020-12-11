d8012dc4216ac8e9cf6f9aa902bbc07e44e1901f
sahejagarwal2008 <72128226+sahejagarwal2008@users.noreply.github.com>
2020-12-11 14:02:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var gameState = 0
+var playerCount,database
+var player,game,form
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  
+}
+
+function draw(){
+  background("white");
+  
+   
+  
+}
+
